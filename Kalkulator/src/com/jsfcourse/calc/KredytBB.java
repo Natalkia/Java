0456de6594ee9700e8e84f5c6a6f693d294f8ae3
Natalkia <46799016+Natalkia@users.noreply.github.com>
2020-10-19 20:41:37 +0200

    Add files via upload

---
+++ Kalkulator/src/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,76 @@
+package com.jsfcourse.calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+
+public class KredytBB {
+	private String kwota;
+	private String oprocentowanie;
+	private Double rata;
+
+	
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOprocentowanie() {
+		return oprocentowanie;
+	}
+
+	public void setOprocentowanie(String oprocentowanie) {
+		this.oprocentowanie = oprocentowanie;
+	}
+
+	
+	public Double getRata() {
+		return rata;
+	}
+
+	public void setRata(Double rata) {
+		this.rata = rata;
+	}
+
+	
+	
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double oprocentowanie = Double.parseDouble(this.oprocentowanie);
+
+			rata =( (oprocentowanie * kwota)/ 100) ;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "OK", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³¹d ", null));
+			return false;
+		}
+	}
+
+
+
+	@Inject
+	FacesContext ctx;
+
+	public String calc() {
+		
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+		
+	}
+
+}
